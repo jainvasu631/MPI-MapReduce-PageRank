b89ae946beb5ff7beab2d211abe661794b308726
Vasu Jain <jain.vasu631@gmail.com>
2020-03-15 16:49:49 +0530

    Added PageRank.cpp

---
+++ PageRank.cpp
@@ -0,0 +1,20 @@
+#include<cmath>
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+typedef vector<float> Column;
+typedef vector<vector<float>> Matrix;
+
+// Method to create the Hyperlink Matrix
+Matrix createHyperlinkMatrix(string filename){
+
+}
+
+// The main method
+int main(int argc, char const *argv[]){   
+    return 0;
+}
+
