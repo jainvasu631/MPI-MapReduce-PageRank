9fb1196bbc7dcbea61a6f52123ffa40cd6aedacc
jainvasu631 <jain.vasu631@gmail.com>
2020-03-15 21:47:47 +0530

    Changed global.h

--- global.h
+++ global.h
@@ -1,10 +1,24 @@
-#include <chrono>
-#include "Classes.h"
+// STL Includes
+#include<chrono>
 
-using namespace std;
-using namespace std::chrono;
+// Class Includes
+#include "Graph.h"
+#include "FlatMatrix.h"
+#include "SparseMatrix.h"
+
+// Type aliases
+using Size = unsigned int;
+using Column = vector<double>;
+using Matrix = vector<Column>;
 
 // Global Constants
 
 // Assign the global constants
 inline void assignConstants();
+
+// Method to create the Hyperlink Matrix
+SparseMatrix createHyperlinkMatrix(Graph& graph){
+    const Size N = graph.numVertices();
+    SparseMatrix result;
+    return result;
+};
