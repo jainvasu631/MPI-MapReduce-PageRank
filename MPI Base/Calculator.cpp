6dca99781e77b761e80c87b05d9660e0a3d45f0d
jainvasu631 <jain.vasu631@gmail.com>
2020-03-18 22:30:30 +0530

    PageRank.cpp working only on Single Processor.

--- MPI Base/Calculator.cpp
+++ MPI Base/Calculator.cpp
@@ -22,21 +22,27 @@ class Calculator{
         const Graph::ToList& toList;
         const Column& Hyperlinks;
         Column& PageRanks;
-        
-        // Function to get the (Key,Value) data
+        Value factor;
+        Value norm;
+
+        // Function to get the Factor (Key,Value) data
         inline Value const getFactorValue(const Graph::Vertex& key) {return ((Hyperlinks[key]>0)? Constant::BETA : Constant::GAMMA) * PageRanks[key];}
+        
+        // Function to get the PageRank (Key,Value) data
+        inline Value const getPageRankValue(const Graph::Vertex& key) {return Hyperlinks[key]* PageRanks[key];}
         
     public:
         // Main Constructor
         Calculator(const Column& hyperlinks, const Graph::ToList& tList, Column& pageRanks) 
             : N(tList.size()), toList(tList), Hyperlinks(hyperlinks), PageRanks(pageRanks){}
         // Function to refresh the Class for another recalculation.
-        inline void refresh(Column& pageRanks) {PageRanks = pageRanks;}
+        inline void refresh(Column& pageRanks) {PageRanks = pageRanks;norm=0;}
         
         // SIZEOF Constants
         static constexpr Graph::Vertex COMMON = 0;
         static constexpr int VERTEX_SIZE = sizeof(Graph::Vertex);
         static constexpr int VALUE_SIZE = sizeof(Value);
+        static constexpr int INT_SORT = 1;
         
         // Map Function for Factor Calculation
         static void MapFactor(Graph::Vertex key, KeyValue* keyvalue, void* calculator){
@@ -47,10 +53,40 @@ class Calculator{
         
         // Reduce Function for Factor Calculation
         static void ReduceFactor(char* key, int keybytes, char* multivalue, int nvalues, int* valuebytes, KeyValue* keyvalue, void* calculator){ 
-            cout << ((nvalues==0)? "Trouble" : "Phew") <<endl;
+            // cout << ((nvalues==0)? "Trouble" : "Phew") <<endl;
             Value* values = (Value*) multivalue;
-            ((Calculator*) calculator)->factor = accumulate(values,values+nvalues,0.0);
+            ((Calculator*) calculator)->factor = accumulate(values,values+nvalues,0.0)/nvalues;
         }
 
-        Value factor; 
+        inline Value& getFactor() {return factor;}
+        inline Value& getNorm() {return norm;}
+
+        // Map Function for Factor Calculation
+        static void MapPageRank(Graph::Vertex key, KeyValue* keyvalue, void* calculator){
+            Calculator* calc = (Calculator*) calculator;
+            Value value = calc->factor;
+            keyvalue->add((char*)&key, VERTEX_SIZE,(char *)&value,VALUE_SIZE);
+            for (const Graph::Vertex& to : calc->toList[key]) {
+                value = calc->getPageRankValue(key);    
+                keyvalue->add((char*)&to, VERTEX_SIZE,(char *)&value,VALUE_SIZE);
+            }
+        }
+        
+        // Reduce Function for Factor Calculation
+        static void ReducePageRank(char* key, int keybytes, char* multivalue, int nvalues, int* valuebytes, KeyValue* keyvalue, void* calculator){ 
+            // cout << ((nvalues==0)? "Trouble" : "Phew") <<endl;
+            Value* values = (Value*) multivalue;
+            Value new_pageRank = Constant::ALPHA *accumulate(values,values+nvalues,0.0);
+            keyvalue->add(key, VERTEX_SIZE,(char *)&new_pageRank,VALUE_SIZE);
+        }
+
+        // Gather Function for putting PageRanks Back
+        static void GatherPageRank(uint64_t index,char* key, int keybytes, char* value, int valuebytes, KeyValue* keyvalue, void* calculator){ 
+            Calculator* calc = (Calculator*) calculator;
+            Value new_pageRank = *((Value*) value);
+            Value& old_pageRank = calc->PageRanks[*((Graph::Vertex*) key)]; // The old value's place
+            calc->norm+=abs(old_pageRank-new_pageRank); // Change norm
+            old_pageRank=new_pageRank; // Replace
+        }
+    friend class PageRank;
 };
--- MPI Base/PageRank.cpp
+++ MPI Base/PageRank.cpp
@@ -19,33 +19,43 @@ using namespace std;
 using namespace chrono;
 using namespace MAPREDUCE_NS;
 
+// MPI Constants
+int RANK,SIZE,HOME=1;
+
 class PageRank{
     private:
-        static Value calculateFactor(Column& pageRanks, Calculator& calculator){
-            MPI_Init(NULL,NULL);
-            int RANK,SIZE,HOME=1;
-            MPI_Comm_rank(MPI_COMM_WORLD,&RANK);
-            MPI_Comm_size(MPI_COMM_WORLD,&SIZE);
-            cout<<"Proc " << RANK<< " is responding"<<endl; 
-            MapReduce mapreduce(MPI_COMM_WORLD);
-            cout<<"MapReduce created"<<endl;
-            Graph::Size N = pageRanks.size();            
+        static Value& calculateFactor(Calculator& calculator, MapReduce& mapreduce){
+            Graph::Size N = calculator.PageRanks.size();            
             MPI_Barrier(MPI_COMM_WORLD);
 
-            // Map Part
-            auto kvPairs = mapreduce.map(N,Calculator::MapFactor,((void*)&calculator));
+            auto kvPairs = mapreduce.map(N,Calculator::MapFactor,((void*)&calculator));// Map Part
             mapreduce.collate(NULL);
             
-            // Reduce Part
-            auto kvmPairs = mapreduce.reduce(Calculator::ReduceFactor,((void*)&calculator));
+            auto kvmPairs = mapreduce.reduce(Calculator::ReduceFactor,((void*)&calculator));// Reduce Part
             MPI_Barrier(MPI_COMM_WORLD);
-            
-            mapreduce.gather(HOME);
-            MPI_Finalize();
-            return calculator.factor/N;
+            return calculator.getFactor();
         }
 
-        static Value performIteration(Column& pageRanks);
+        static Value performIteration(Column& pageRanks, Calculator& calculator, MapReduce& mapreduce){
+            calculator.refresh(pageRanks);
+            Value& value = calculateFactor(calculator,mapreduce);
+            
+            const Graph::Size N = pageRanks.size();
+            
+            MPI_Bcast((void*) &value, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+            MPI_Bcast(pageRanks.data(), N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+            
+            auto kvPairs = mapreduce.map(N,Calculator::MapPageRank,((void*)&calculator));// Map Part
+            mapreduce.collate(NULL);
+            
+            auto kvmPairs = mapreduce.reduce(Calculator::ReducePageRank,((void*)&calculator));// Reduce Part
+            MPI_Barrier(MPI_COMM_WORLD);
+            mapreduce.gather(HOME);
+            mapreduce.sort_keys(Calculator::INT_SORT);
+            kvPairs = mapreduce.map(&mapreduce,Calculator::GatherPageRank,((void*)&calculator));// Gather Part
+            MPI_Barrier(MPI_COMM_WORLD);
+            return calculator.getNorm();
+        }
     
     public:
         // Find PageRank      
@@ -53,11 +63,14 @@ class PageRank{
             Column pageRanks = Utility::getInitPageRank(graph.numVertices()); // This will contain the PageRanks at the end of the function
             Value factor; // This will be added to help converge PageRanks Column
             const Column hyperlinks = Utility::calculateHyperLinkColumn(graph.toList); 
+            Calculator calculator(hyperlinks,graph.toList,pageRanks);
+            MapReduce mapreduce(MPI_COMM_WORLD);
             Value norm;
             unsigned int iteration=1;
             // Main Iteration
             do{
                 cout<< "Performing "<< iteration++ <<" Iteration.";
+                norm = performIteration(pageRanks,calculator,mapreduce);
                 cout<<" Current norm is "<< norm << endl;
             } while(norm>Constant::TOL);
             return pageRanks;
@@ -67,27 +80,29 @@ class PageRank{
             Graph graph = Utility::timedGraphCreation(filename);
             // graph.print();
 
-            const Column hyperlinks = Utility::calculateHyperLinkColumn(graph.toList);
-            Column pageRanks = Utility::getInitPageRank(hyperlinks.size());
-            Calculator calculator(hyperlinks,graph.toList,pageRanks);
-            Value factor = PageRank::calculateFactor(pageRanks,calculator);
-            
-            cout<<"The factor is " << factor << endl;
             // PageRank Algorithm
-            // auto start_pageRank_algorithm = high_resolution_clock::now();
-            // const Column pageRanks = calculatePageRank(graph);
-            // auto end_pageRank_algorithm = high_resolution_clock::now();
-            // auto pageRank_algorithm_duration = duration_cast<milliseconds>(end_pageRank_algorithm- start_pageRank_algorithm);
-            // cout << "PageRank Algorithm took " << pageRank_algorithm_duration.count()<<"ms"<<endl;
-            // Utility::printPageRank(pageRanks);
+            auto start_pageRank_algorithm = high_resolution_clock::now();
+            const Column pageRanks = calculatePageRank(graph);
+            auto end_pageRank_algorithm = high_resolution_clock::now();
+            auto pageRank_algorithm_duration = duration_cast<milliseconds>(end_pageRank_algorithm- start_pageRank_algorithm);
+            cout << "PageRank Algorithm took " << pageRank_algorithm_duration.count()<<"ms"<<endl;
+            Utility::printPageRank(pageRanks);
         }        
 };
 
 
 // The main method
 int main(int argc, char const *argv[]){   
+    MPI_Init(NULL,NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD,&RANK);
+    MPI_Comm_size(MPI_COMM_WORLD,&SIZE);
+    
+    
     cout << argv[argc-1] << endl;
     string filename = argv[argc-1];
     PageRank::test(filename);
+    
+    
+    MPI_Finalize();
     return 0;
 }
