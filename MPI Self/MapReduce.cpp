44ce19f271c1722c14f4ed4fb4a3f9768dc13cd2
jainvasu631 <jain.vasu631@gmail.com>
2020-03-28 11:00:19 +0530

    Added MapReduce.cpp with class prototypes

---
+++ MPI Self/MapReduce.cpp
@@ -0,0 +1,32 @@
+#include<vector>
+#include<mpi.h>
+#include<algorithm>
+
+using namespace std;
+
+// Temporary typenames because using templates is annoying
+using MapKey = int;
+using MapValue = int;
+using ReduceKey = int;
+using ReduceValue = int;
+using ResultKey = int;
+using ResultValue = int;
+
+class MapTask{
+
+};
+class Combiner{
+
+};
+
+class ReduceTask{
+
+};
+
+class DataSource{
+
+};
+
+class Results{
+
+};
