319db40b5969aae345a0a5f33f19d763a4eeccfa
jainvasu631 <jain.vasu631@gmail.com>
2020-03-28 15:44:08 +0530

    Defined the Task Template for Map and Reduce

--- MPI Self/Include/MapReduce.hpp
+++ MPI Self/Include/MapReduce.hpp
@@ -1,5 +1,5 @@
 #include<vector>
-#include<mpi.h>
+// #include<mpi.h>
 #include<algorithm>
 #include<unordered_map>
 
@@ -19,14 +19,46 @@ using CombinerTuple = pair<ReduceKey, ReduceValue>;
 using ReduceTuple = pair<ReduceKey, vector<ReduceValue>>;
 using ResultTuple = pair<ResultKey, ResultValue>;
 
-template<typename Input, typename Results>
+template<typename T1, typename T2>
 class Runnable{
     public:
-        explicit Runnable(const Input& input_): input(input_){}
-    private:
+        // Forced Aliasing so that Child Classes can inherit this
+        using Input = T1; 
+        using Results = T2;
+
+        // Explicit Constructor
+        explicit Runnable(const Input& input_): input(input_), results(0){}
+    protected:
         Results results; // Stores results of Runnable Function
         Input& input; // Stores Input of Runnable Function
-    protected:
         // Runner Function
         void run();
+};
+
+template<typename T1, typename T2, typename T3, typename T4>
+class Task {
+    public:
+        // Alias Tuple Definitions
+        using InputKey = T1;
+        using InputValue = T2;
+        using ResultKey = T3;
+        using ResultValue = T4;
+        
+        using InputTuple = pair<T1,T2>;
+        using ResultTuple = pair<T3,T4>;       
+        
+        using Input = vector<InputTuple>; 
+        using Results = vector<ResultTuple>;
+        
+        // Constructor
+        explicit Task(const Input& input_): input(input_), results(0){}
+
+        // User Implemented Operator    
+        void operator()(const InputKey& key, const InputValue& value, Task::Results& results);
+    protected:
+        Results results; // Stores results of Runnable Function
+        Input& input; // Stores Input of Runnable Function
+        
+        // Iterates over input and runs the MapFunction
+        void run(){for(auto& tuple: input) operator()(tuple.first,tuple.second,results);}
 };
