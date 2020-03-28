84abb86a91e2c35d3a2efffa412d1f6c083e4b8c
jainvasu631 <jain.vasu631@gmail.com>
2020-03-28 16:14:29 +0530

    Transferred Runnable and Task to Runnable

--- MPI Self/Include/MapReduce.hpp
+++ MPI Self/Include/MapReduce.hpp
@@ -1,7 +1,11 @@
+#pragma once
+
 #include<vector>
 // #include<mpi.h>
 #include<algorithm>
 #include<unordered_map>
+
+#include "Runnable.cpp"
 
 using namespace std;
 
@@ -18,47 +22,3 @@ using MapTuple = pair<MapKey,MapValue>;
 using CombinerTuple = pair<ReduceKey, ReduceValue>;
 using ReduceTuple = pair<ReduceKey, vector<ReduceValue>>;
 using ResultTuple = pair<ResultKey, ResultValue>;
-
-template<typename T1, typename T2>
-class Runnable{
-    public:
-        // Forced Aliasing so that Child Classes can inherit this
-        using Input = T1; 
-        using Results = T2;
-
-        // Explicit Constructor
-        explicit Runnable(const Input& input_): input(input_), results(0){}
-    protected:
-        Results results; // Stores results of Runnable Function
-        Input& input; // Stores Input of Runnable Function
-        // Runner Function
-        void run();
-};
-
-template<typename T1, typename T2, typename T3, typename T4>
-class Task {
-    public:
-        // Alias Tuple Definitions
-        using InputKey = T1;
-        using InputValue = T2;
-        using ResultKey = T3;
-        using ResultValue = T4;
-        
-        using InputTuple = pair<T1,T2>;
-        using ResultTuple = pair<T3,T4>;       
-        
-        using Input = vector<InputTuple>; 
-        using Results = vector<ResultTuple>;
-        
-        // Constructor
-        explicit Task(const Input& input_): input(input_), results(0){}
-
-        // User Implemented Operator    
-        void operator()(const InputKey& key, const InputValue& value, Task::Results& results);
-    protected:
-        Results results; // Stores results of Runnable Function
-        Input& input; // Stores Input of Runnable Function
-        
-        // Iterates over input and runs the Operator() Function
-        void run(){for(auto& tuple: input) operator()(tuple.first,tuple.second,results);}
-};
---
+++ MPI Self/Include/Runnable.cpp
@@ -0,0 +1,48 @@
+#pragma once
+#include<vector>
+
+using namespace std;
+
+template<typename T1, typename T2>
+class Runnable{
+    public:
+        // Forced Aliasing so that Child Classes can inherit this
+        using Input = T1; 
+        using Results = T2;
+
+        // Explicit Constructor
+        explicit Runnable(const Input& input_): input(input_), results(0){}
+    protected:
+        Results results; // Stores results of Runnable Function
+        Input& input; // Stores Input of Runnable Function
+        // Runner Function
+        void run();
+};
+
+template<typename T1, typename T2, typename T3, typename T4>
+class Task {
+    public:
+        // Alias Tuple Definitions
+        using InputKey = T1;
+        using InputValue = T2;
+        using ResultKey = T3;
+        using ResultValue = T4;
+        
+        using InputTuple = pair<T1,T2>;
+        using ResultTuple = pair<T3,T4>;       
+        
+        using Input = vector<InputTuple>; 
+        using Results = vector<ResultTuple>;
+        
+        // Constructor
+        explicit Task(const Input& input_): input(input_), results(0){}
+
+        // User Implemented Operator    
+        void operator()(const InputKey& key, const InputValue& value, Task::Results& results);
+    protected:
+        Results results; // Stores results of Runnable Function
+        Input& input; // Stores Input of Runnable Function
+        
+        // Iterates over input and runs the Operator() Function
+        void run(){for(auto& tuple: input) operator()(tuple.first,tuple.second,results);}
+};
