f43fe07475d96832d86c830b9aa8715cf02e6a11
jainvasu631 <jain.vasu631@gmail.com>
2020-03-28 16:55:05 +0530

    Removed Helper Class and Moved Map And Reduce to MapReduce Class inside MapReduce.hpp

--- MPI Self/Include/MapReduce.cpp
+++ MPI Self/Include/MapReduce.cpp
@@ -1,24 +1,4 @@
 #include"MapReduce.hpp"
 using namespace std;
 
-// Map Class
-// This Map operator() is Implemented by User 
-// User is passed Key and Value by reference.
-// It does (k1,v1)->list(k2,v2) where (k2,v2) are created as pairs and added to results 
-class MapTask: Task<MapKey,MapValue,ReduceKey,ReduceValue> {
-    public: using Task::Task;
-};
-
-// Reduce Class
-// This Reduce operator() is Implemented by User 
-// User is passed Key and vector of Values by reference.
-// It does (k2,list(v2))->list(k3,v3) where (k3,v3) are created as pairs and added to results
-class ReduceTask : Task<ReduceKey,vector<ReduceValue>,ResultKey,ResultValue>{
-    public: using Task::Task;
-};
-
-int main(int argc, char const *argv[])
-{
-    /* code */
-    return 0;
-}
+// Implementation of MapReduce Class Functions
--- MPI Self/Include/MapReduce.hpp
+++ MPI Self/Include/MapReduce.hpp
@@ -6,19 +6,69 @@
 #include<unordered_map>
 
 #include "Runnable.cpp"
+#include "DataSource.cpp"
 
 using namespace std;
 
 // Temporary typenames because using templates is annoying
-using MapKey = int;
-using MapValue = int;
-using ReduceKey = int;
-using ReduceValue = int;
-using ResultKey = int;
-using ResultValue = int;
+// using MapKey = int;
+// using MapValue = int;
+// using ReduceKey = int;
+// using ReduceValue = int;
+// using ResultKey = int;
+// using ResultValue = int;
 
-// Tuple typenames
-using MapTuple = pair<MapKey,MapValue>;
-using CombinerTuple = pair<ReduceKey, ReduceValue>;
-using ReduceTuple = pair<ReduceKey, vector<ReduceValue>>;
-using ResultTuple = pair<ResultKey, ResultValue>;
+template<typename MapKey, typename MapValue, typename ReduceKey, typename ReduceValue, typename ResultKey, typename ResultValue>
+class MapReduce{
+    public:
+        // Tuple typenames
+        using MapTuple = pair<MapKey,MapValue>;
+        using CombinerTuple = pair<ReduceKey, ReduceValue>;
+        using ReduceTuple = pair<ReduceKey, vector<ReduceValue>>;
+        using ResultTuple = pair<ResultKey, ResultValue>;
+
+        // Map Class
+        // This Map operator() is Implemented by User 
+        // User is passed Key and Value by reference.
+        // It does (k1,v1)->list(k2,v2) where (k2,v2) are created as pairs and added to results 
+        using MapTask = Task<MapKey,MapValue,ReduceKey,ReduceValue>;
+
+        // Reduce Class
+        // This Reduce operator() is Implemented by User 
+        // User is passed Key and vector of Values by reference.
+        // It does (k2,list(v2))->list(k3,v3) where (k3,v3) are created as pairs and added to results
+        using ReduceTask = Task<ReduceKey,vector<ReduceValue>,ResultKey,ResultValue>;
+
+        // Source class
+        using Source = DataSource<MapKey,MapValue>;
+
+        // Combiner Class to combine results from Map Class
+        class Combiner: Runnable<vector<CombinerTuple>,unordered_map<ReduceKey,vector<ReduceValue>>>{
+            // Iterates over all Key-Value tuples and Combines those Tuples which have Same Key into a List
+            protected: void run(){for(auto& tuple : this->input) this->results[tuple.first].push_back(tuple.second);}
+        };
+
+        // Distributor Class to Distribute results to different Reduce Class
+        class Distributor: Runnable<unordered_map<ReduceKey,vector<ReduceValue>>, vector<ReduceTuple>>{
+            // Converts the Map into a Vector to Save Memory Probably extremely inefficient
+            // Will also do other || processing stuff later
+            protected: void run() {this->results = Results(this->input.begin(),this->input.end());}
+        };
+
+        // Class to Aggregate Results From ReduceTask
+        class Results{
+            private:
+
+            public:
+        };
+
+
+    protected:
+        MapTask map;
+        ReduceTask reduce;
+        Source source;
+        Combiner combiner;
+        Distributor distributor;
+}; 
+
+
