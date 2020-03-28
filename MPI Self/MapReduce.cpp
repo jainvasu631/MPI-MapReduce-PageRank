8973e58bc28df9a26e4c45451a6dbdbacd971961
jainvasu631 <jain.vasu631@gmail.com>
2020-03-28 11:28:45 +0530

    Wrote basic prototype of MapTask Class

--- MPI Self/MapReduce.cpp
+++ MPI Self/MapReduce.cpp
@@ -13,14 +13,32 @@ using ResultKey = int;
 using ResultValue = int;
 
 class MapTask{
-
+    
+    public:
+        using Results = vector<pair<ReduceKey,ReduceValue>>;
+        using Input = vector<pair<MapKey, MapValue>>;    
+    
+        // This Map Function is Implemented by User 
+        // User is passed Key and Value by reference.
+        // It does (k1,v1)->list(k2,v2) where (k2,v2) are created as pairs and added to results
+        void operator()(const MapKey& key, const MapValue& value, Results& results);
+    private:
+        Results results; // Stores results of Map Function
+        Input input; // Stores Input of Map Function
+    protected:
+        // Iterates over input and runs the MapFunction
+        void run(){for(auto& tuple: input) operator()(tuple.first,tuple.second,results);}
 };
+
 class Combiner{
 
 };
 
 class ReduceTask{
-
+    private:
+        using Results = vector<pair<ResultKey,ResultValue>>;
+        using Input = vector<pair<ResultKey, ResultValue>>;
+    public:
 };
 
 class DataSource{
@@ -28,5 +46,7 @@ class DataSource{
 };
 
 class Results{
+    private:
 
+    public:
 };
