03d92b97c17fa64959ff689e29c619d04d995b5a
jainvasu631 <jain.vasu631@gmail.com>
2020-03-29 16:04:38 +0530

    Wrote basic workflow for how MapReduce will work

--- MPI Self/Include/MapReduce.cpp
+++ MPI Self/Include/MapReduce.cpp
@@ -1,4 +1,7 @@
 #include"MapReduce.hpp"
 using namespace std;
 
+
+// Workflow of MapReduce Class
+// MapDistributor (calls) -> MapTask -> Combiner -> Reduce Distributor -> ReduceTask -> Output 
 // Implementation of MapReduce Class Functions
