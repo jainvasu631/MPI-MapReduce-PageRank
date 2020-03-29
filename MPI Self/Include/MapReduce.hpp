41fe6cbf489666cf056d789e75b0807c4d8764e2
jainvasu631 <jain.vasu631@gmail.com>
2020-03-29 16:57:51 +0530

    Wrote the Workload Function

--- MPI Self/Include/MapReduce.hpp
+++ MPI Self/Include/MapReduce.hpp
@@ -36,6 +36,7 @@ class MapReduce{
 
         // Class to Generate Data
         class Generator: Runnable<DataSource,vector<MapTuple>>{    
+            
             // User defined function. Where user is passed reference to Key,Value and a KeyId
             public: bool const getData(int keyId, MapKey& key, MapValue& value);
             
@@ -68,10 +69,35 @@ class MapReduce{
             public:
         };
 
+        // Explicit Constructor
+        MapReduce(DataSource& data) : generator(data) {MPI_Comm_rank(MPI_COMM_WORLD,&Rank);MPI_Comm_size(MPI_COMM_WORLD,&Size);}
+
+    private:
+        int Rank,Size;
+        
+        using Workload = pair<unsigned int, unsigned int>; // Will store information about the Offset and Individual Process Keys
+
+        // Used by each thread/process to find its range of work
+        Workload getProcessLoad(int totalKeys){
+            int numkeys = (totalKeys/Size);
+            int offset = Rank*numkeys;
+            int process_numKeys = (Rank==Size-1)? totalKeys-(Rank-1)*numkeys : numkeys;
+            return Workload(offset, process_numkeys);
+        }
 
     protected:
         Combiner combiner;
         Distributor distributor;
+        Generator generator;
+
+        Output run(){
+            int totalKeys;
+            Workload workload = getProcessLoad(totalKeys);
+            generator.run(Workload.first,Workload.second);
+            MapTask maptask(generator.getResults());
+            maptask.run();
+            
+        }
 }; 
 
 
